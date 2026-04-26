261a93c60920cb656d4c01f211fcbbb7a8c95582
Kalpanath <kalpanathofficial@gmail.com>
2026-04-26 11:18:54 +0530

    js scroll link highlight

---
+++ Js/script.js
@@ -0,0 +1,23 @@
+const sections = document.querySelectorAll("section[id]");
+const navLinks = document.querySelectorAll(".nav-links a");
+
+window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        if (window.scrollY >= sectionTop) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove("active");
+        if (link.getAttribute("href") === `#${current}`) {
+            link.classList.add("active");
+        }
+    });
+});
+
+
+
